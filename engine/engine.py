71fd60511caa62cd6a10c5cffd5d9dd8810101f9
George Nijo <george.nijo8@gmail.com>
2022-03-21 22:13:28 -0400

    test classes added

--- engine/engine.py
+++ engine/engine.py
@@ -1,8 +1,10 @@
-from car import Car
+from engine import Engine
 
-class Engine(Car):
-    def __init__(self):
-        self = self
 
-    def needs_service(self):
-        pass
+class CapuletEngine(Engine):
+    def __init__(self, current_mileage, last_service_mileage):
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_serive(self):
+        return self.current_mileage - self.last_service_mileage > 30000
